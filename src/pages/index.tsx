030d25100372a145de235b4bab1efffa4546afe4
troyaks <lgotcfg@gmail.com>
2023-05-29 20:58:22 -0300

    Predefined places for search bar, login and logo. Now need to finish logo and login design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,28 @@ import { BsSearch } from "react-icons/bs";
 const Home: NextPage = () => {
   return (
     <div className='flex flex-col min-h-screen w-full'>
-      <div className='bg-blue-600 top-0 z-50 px-8 sticky box-border block'>
-        <div className='py-0.5 w-full box-border'>
-          <div className='justify-between w-full items-center flex m-1'>
-            {/* search bar */}
-            <div className='w-fit flex max-w-xs h-8 relative items-stretch'>
-              <input className='text-white px-8 text-sm font-light rounded-xl outline-none' placeholder='Busque por produtos!' />
-              <BsSearch className='absolute right-3 top-2' />
-            </div>
+      {/* Header */}
+      <div className='bg-blue-600 top-0 z-50 px-4 py-0.5 sticky box-border w-full flex justify-between items-center'>
+        {/* logo */}
+        <div className='absolute mr-auto'>
+          LOGO
+        </div>
+        {/* search bar */}
+        <div className='flex relative left-1/2 transform -translate-x-1/2'>
+          <div className='flex max-w-xs h-8 items-stretch'>
+            <input className='text-white px-8 text-sm font-light rounded-xl outline-none' placeholder='Busque por produtos!' />
+            <BsSearch className='absolute right-3 top-2' />
+          </div>
+        </div>
+        {/* Name and Login */}
+        <div className='flex items-center'>
+          {/* Name */}
+          <div className='p-4'>
+            NAME
+          </div>
+          {/* login */}
+          <div className='ml-auto'>
+            LOGIN
           </div>
         </div>
       </div>
