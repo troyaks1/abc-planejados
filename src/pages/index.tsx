5b18ca4ba3a1d38b8fb6a1ded2bb572aa091fa5f
troyaks <lgotcfg@gmail.com>
2023-05-29 15:06:34 -0300

    Finished search bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { BsSearch } from "react-icons/bs";
 
 const Home: NextPage = () => {
   return (
     <div className='flex flex-col min-h-screen w-full'>
-      <div className='bg-gray-400 top-0 z-50 px-8 sticky box-border block'>
+      <div className='bg-blue-600 top-0 z-50 px-8 sticky box-border block'>
         <div className='py-0.5 w-full box-border'>
-          <div className='justify-between w-full items-center flex m-5'>
+          <div className='justify-between w-full items-center flex m-1'>
             {/* search bar */}
-            <div className='w-full flex cursor-pointer overflow-hidden max-w-xs h-6 relative'>
-              <div className='mr-1 items-stretch flex'>
-                <input className='text-black pl-6 text-sm font-light rounded-lg'>
-
-                </input>
-              </div>
+            <div className='w-fit flex max-w-xs h-8 relative items-stretch'>
+              <input className='text-white px-8 text-sm font-light rounded-xl outline-none' placeholder='Busque por produtos!' />
+              <BsSearch className='absolute right-3 top-2' />
             </div>
           </div>
         </div>
