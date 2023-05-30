86047d5aae5a3d63da8e027bb616725303fd058f
troyaks <lgotcfg@gmail.com>
2023-05-30 13:25:34 -0300

    Finished header and did some styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,38 +5,41 @@ import styles from '../styles/Home.module.css'
 import { BsSearch, BsPersonCircle } from "react-icons/bs";
 
 const Home: NextPage = () => {
+
+  const bgColorPrimary = 'bg-slate-200';
+  const textColorPrimary = 'text-zinc-800';
+  const bgColorSecundary = 'bg-slate-400'
+
   return (
-    <div className='flex flex-col min-h-screen w-full'>
+    <div className={`${bgColorPrimary} flex flex-col min-h-screen w-full`}>
       {/* Header */}
-      <div className='bg-blue-600 top-0 z-50 px-4 sticky box-border w-full flex justify-between items-center'>
+      <div className={`${bgColorPrimary} top-0 z-50 px-12 py-3 sticky box-border w-full flex justify-between items-center rounded-b-full shadow-xl`}>
         {/* logo */}
-        <div className='absolute mr-auto'>
-          LOGO
+        <div className={`absolute mr-auto text-2xl ${textColorPrimary} font-bold`}>
+          planejados.abc.br
         </div>
         {/* search bar */}
-        <div className='flex relative left-1/2 transform -translate-x-1/2'>
-          <div className='flex max-w-xs h-8 items-stretch'>
-            <input className='text-white px-8 text-sm font-light rounded-xl outline-none' placeholder='Busque por produtos!' />
-            <BsSearch className='absolute right-3 top-2' />
+        <div className='flex relative left-1/2 transform -translate-x-1/2 '>
+          <div className={`flex max-w-xs h-10 items-stretch rounded-full overflow-hidden shadow-xl`}>
+            <input className={`w-full ${bgColorSecundary} ${textColorPrimary} font-bold px-10 text-sm rounded-l-full outline-none`} />
+            <BsSearch className={`absolute right-3 top-2.5 ${textColorPrimary}`} size={20}/>
           </div>
         </div>
         {/* Name and Login */}
-        <div className='flex items-center'>
+        <div className='flex items-center space-x-4'>
           {/* Name */}
-          <div className='p-4'>
+          <div className={`${textColorPrimary}`}>
             Olá, cliente!
           </div>
           {/* login */}
-          <div className='ml-auto flex flex-row cursor-pointer items-center'>
-            <div className='p-2'>
-              <BsPersonCircle size={30}/>
-            </div>
-            <div className='flex flex-col text-xs'>
-              <div>
-              Faça login
+          <div className='ml-auto flex flex-row cursor-pointer items-center space-x-2'>
+            <BsPersonCircle size={30} className={`${textColorPrimary}`} />
+            <div className='flex flex-col text-xs text-white'>
+              <div className={`${textColorPrimary}`}>
+                Faça login
               </div>
-              <div className='font-bold'>
-              ou cadastre-se
+              <div className={`${textColorPrimary} font-bold`}>
+                ou cadastre-se
               </div>
             </div>
           </div>
@@ -44,6 +47,7 @@ const Home: NextPage = () => {
       </div>
     </div>
   )
+
 }
 
 export default Home
