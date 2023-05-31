b6afabb4dd8e3fc8f25ae5b56d5ce35bc944bc55
troyaks <lgotcfg@gmail.com>
2023-05-31 00:23:33 -0300

    Started creating the footer for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
 import Header, { HeaderStyle } from '../components/Header.cmp';
+import { BsCart, BsWhatsapp, BsPersonCircle } from 'react-icons/bs';
+import { BiStoreAlt } from 'react-icons/bi';
 
 
 const Home: NextPage = () => {
@@ -15,7 +17,7 @@ const Home: NextPage = () => {
         text: 'text-zinc-800',
       },
       terceary: {
-        bg: 'bg-red-200',
+        bg: 'bg-red-300',
         text: 'text-zinc-800'
       }
     }
@@ -24,6 +26,20 @@ const Home: NextPage = () => {
   return (
     <div className={`${headerStyle.color.primary.bg} flex flex-col min-h-screen w-full`}>
       <Header color={headerStyle.color} />
+      <div className={`${headerStyle.color.terceary.bg} ${headerStyle.color.terceary.text} w-full p-2 bottom-0 absolute flex flex-row justify-around items-center lg:hidden`}>
+        <div>
+          <BiStoreAlt size={24}/>
+        </div>
+        <div>
+          <BsWhatsapp size={24}/>
+        </div>
+        <div>
+          <BsCart size={24}/>
+        </div>
+        <div>
+          <BsPersonCircle size={24}/>
+        </div>
+      </div>
     </div>
   )
 
