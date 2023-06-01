9258c62ce2584d86e4bc2a84debd2df8d978919f
troyaks <lgotcfg@gmail.com>
2023-06-01 09:18:01 -0300

    Started creation of midModal yet only for mobile

--- src/components/Footer.cmp.tsx
+++ src/components/Footer.cmp.tsx
@@ -1,7 +1,8 @@
-import { BsCart, BsWhatsapp, BsPersonCircle, BsList, BsListNested, BsListUl, BsReverseListColumnsReverse } from 'react-icons/bs';
-import { BiStoreAlt } from 'react-icons/bi';
+import { BsCart, BsWhatsapp, BsPersonCircle } from 'react-icons/bs';
+import { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import Color from '@/interfaces/frontend/Color';
+import MidModal from './MidModal.cmp';
 
 interface Props {
   color: Color
@@ -19,38 +20,57 @@ export default function Footer({ ...props }: Props): JSX.Element {
 }
 
 function FooterForMobile({ ...props }: Props): JSX.Element {
+
+  const [isModalClosed, setModalClosed] = useState<boolean>(true);
+  const [lastOptionChosen, setOptionChosen] = useState<string>('');
+
+  const handleClick = (optChosen: string) => {
+    setOptionChosen(optChosen);
+    if (lastOptionChosen === optChosen || isModalClosed) { setModalClosed(!isModalClosed) }
+  }
+
   return (
-    <div className={`flex ${props.color.secundary.bg} ${props.color.secundary.text} ${props.color.secundary.shadow} w-full bottom-0 pt-1.5 pb-0.5 z-50 px-8 absolute flex flex-row justify-between items-center rounded-t-xl shadow-inner lg:hidden`}>
-      <div className='flex flex-col items-center cursor-pointer'>
-        <div>
-          <BsWhatsapp size={26} />
+    <div className='flex flex-col'>
+      {/* Mid modal */}
+      {!isModalClosed && (
+        <MidModal
+          style={{ color: props.color, type: lastOptionChosen }}
+          action={{ setClosedFromModal: setModalClosed }}
+        />
+      )}
+      {/* Actual Footer */}
+      <div className={`flex ${props.color.secundary.bg} ${props.color.secundary.text} ${props.color.secundary.shadow} w-full bottom-0 pt-1.5 pb-0.5 z-50 px-8 absolute flex flex-row justify-between items-center rounded-t-xl shadow-inner lg:hidden`}>
+        <div className='flex flex-col items-center cursor-pointer'>
+          <div>
+            <BsWhatsapp size={26} onClick={() => handleClick('WhatsApp')}/>
+          </div>
+          <div className='text-xs'>
+            WhatsApp
+          </div>
         </div>
-        <div className='text-xs'>
-          WhatsApp
+        <div className='flex flex-col items-center cursor-pointer'>
+          <div>
+            <BsCart size={26} onClick={() => handleClick('Cart')}/>
+          </div>
+          <div className='text-xs'>
+            Carrinho
+          </div>
         </div>
-      </div>
-      <div className='flex flex-col items-center cursor-pointer'>
-        <div>
-          <BsCart size={26} />
+        <div className='flex flex-col items-center cursor-pointer'>
+          <div>
+            <BsPersonCircle size={26} onClick={() => handleClick('Profile')}/>
+          </div>
+          <div className='text-xs'>
+            Perfil
+          </div>
         </div>
-        <div className='text-xs'>
-          Carrinho
-        </div>
-      </div>
-      <div className='flex flex-col items-center cursor-pointer'>
-        <div>
-          <BsPersonCircle size={26} />
-        </div>
-        <div className='text-xs'>
-          Perfil
-        </div>
-      </div>
-      <div className='flex flex-col items-center cursor-pointer'>
-        <div>
-          <GiHamburgerMenu size={26} />
-        </div>
-        <div className='text-xs'>
-          Listas
+        <div className='flex flex-col items-center cursor-pointer'>
+          <div>
+            <GiHamburgerMenu size={26} onClick={() => handleClick('Menu')}/>
+          </div>
+          <div className='text-xs'>
+            Menu
+          </div>
         </div>
       </div>
     </div>
--- src/components/MidModal.cmp.tsx
+++ src/components/MidModal.cmp.tsx
@@ -1,11 +1,31 @@
 import Color from "@/interfaces/frontend/Color"
+import { Dispatch, SetStateAction } from "react"
+import { AiFillCloseCircle } from 'react-icons/ai'
 
 export interface Props {
   style: {
     color: Color
+    type: string
+  }
+  action: {
+    setClosedFromModal: Dispatch<SetStateAction<boolean>>
   }
 }
 
-export default function MidModal ({...props}: Props) {
+export default function MidModal({ ...props }: Props): JSX.Element {
 
+  const setModalClosed = () => props.action.setClosedFromModal(true);
+
+  return (
+    <div className='flex flex-col lg:hidden'>
+      <div className="flex items-center">
+        <button className="p-1" onClick={() => setModalClosed()}>
+          <AiFillCloseCircle />
+        </button>
+      </div>
+      <div className="flex justify-center items-center">
+        {props.style.type}
+      </div>
+    </div>
+  )
 }
